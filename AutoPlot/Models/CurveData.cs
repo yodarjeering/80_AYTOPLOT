d439d3cc17db0d0239bfd08de39639df7d157963
yodarjeering <yodarjeering429@outlook.com>
2025-12-27 13:27:32 +0900

    add C# files

---
+++ AutoPlot/Models/CurveData.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace AutoPlot.Models
+{
+    public class CurveData
+    {
+        public List<ImagePoint> Points { get; set; } = new();
+    }
+}
---
+++ AutoPlot/Models/ImagePoint.cs
@@ -0,0 +1,8 @@
+namespace AutoPlot.Models
+{
+    public struct ImagePoint
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
