ca77b983843a3fa930dbc95f8b8c2366adb5c17e
yodarjeering <yodarjeering429@outlook.com>
2025-12-31 17:33:43 +0900

    Min Max ダイアログ追加

---
+++ AutoPlot/Models/AxisSettings.cs
@@ -0,0 +1,13 @@
+namespace AutoPlot.Models
+{
+    public class AxisSettings
+    {
+        public double XMin { get; set; }
+        public double XMax { get; set; }
+        public bool IsXLog { get; set; }
+
+        public double YMin { get; set; }
+        public double YMax { get; set; }
+        public bool IsYLog { get; set; }
+    }
+}
